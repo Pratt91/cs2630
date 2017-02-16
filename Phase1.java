37c1203b7b942562bc38d8815e4a14cb865e4bb7
brgquist <alex-bergquist@uiowa.edu>
2017-02-16 13:59:18 -0600

    Update Phase1.java
    Completed Phase 1

--- Phase1.java
+++ Phase1.java
@@ -1,3 +1,4 @@
+// package Minima // this is our package declaration so it would run in our IDE
 package edu.uiowa.cs;
 
 import java.util.ArrayList;
@@ -47,12 +48,14 @@ public class Phase1 {
             }else if(mals.get(i).instruction_id == 100){ // case for blt
                 Instruction step1 = new Instruction(8, 1, mals.get(i).rs, mals.get(i).rt, 0, 0, 0, 0, 0); //breaks mal instr. into 2 steps, this step is the slt part
                 Instruction step2 = new Instruction(6, 0, 1, 0, 0, 0, 0, 0, mals.get(i).branch_label); // this step is the bne part
+                
                 output.add(step1); // adds the 2 steps in order to the output list
                 output.add(step2);
                 
             }else if (mals.get(i).instruction_id == 101){ // case for bge
                 Instruction step1 = new Instruction(8, 1, mals.get(i).rt, mals.get(i).rs, 0, 0, 0, 0, 0); // step for the slt part
                 Instruction step2 = new Instruction(5, 0, 1, 0, 0, 0, 0, 0, mals.get(i).branch_label); // step for the beq part
+                
                 output.add(step1); //adds the 2 steps in order to the output list
                 output.add(step2);
                 
@@ -61,26 +64,6 @@ public class Phase1 {
             }
         }
         return output;
-    }
-    
-    public static void main(String[] args){ //test method
-        List<Instruction> test = new ArrayList<Instruction>();
-        Instruction t1 = new Instruction(0,0,0,0,0,0,0,0,0); // base step
-        Instruction t2 = new Instruction(1,0,9,8,65535,0,0,0,0); //addiu step
-        Instruction t3 = new Instruction(10,0,11,10,65536,0,0,0,0); //ori step
-        Instruction t4 = new Instruction(100,0,16,8,0,0,0,0,3); // blt step
-        Instruction t5 = new Instruction(101,0,17,9,0,0,0,0,4); // bge step
-        
-        test.add(t1);
-        test.add(t2);
-        test.add(t3); 
-        test.add(t4);
-        test.add(t5);
-        
-        List<Instruction> result = new ArrayList<Instruction>();
-        result = mal_to_tal(test);
-        System.out.println(result.toString());
-        
     }
     
 }
