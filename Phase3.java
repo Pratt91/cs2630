8052f2776258705728a7d48106bef72a0b5bef13
brgquist <alex-bergquist@uiowa.edu>
2017-02-16 13:58:42 -0600

    Update Phase3.java
    Completed Phase 3

--- Phase3.java
+++ Phase3.java
@@ -1,5 +1,5 @@
-package Minima;
-// package edu.uiowa.cs;
+// package Minima // this is our package declaration so it would run in our IDE
+package edu.uiowa.cs;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -19,13 +19,14 @@ public class Phase3 {
         ArrayList<Integer> output = new ArrayList<Integer>();
         ArrayList<String> result = new ArrayList<String>();
         
+        
         for(int i = 0; i < tals.size(); i++){
+                        
             if(tals.get(i).instruction_id == 1){ // addiu, I-type, opcode = 9
                 
                 String opcode = Integer.toBinaryString(9);
                 opcode = String.format("%06d", Integer.parseInt(opcode)); // gets us correct number of bits
-                System.out.println(opcode);
-                
+                                
                 String $rs = Integer.toBinaryString(tals.get(i).rs);
                 $rs = String.format("%05d", Integer.parseInt($rs));
                 
@@ -42,46 +43,50 @@ public class Phase3 {
                 String opcode = "000000"; // opcode for R-type
                 
                 String $rs = Integer.toBinaryString(tals.get(i).rs);
-                $rs = String.format("%050d", Integer.parseInt($rs));
+                $rs = String.format("%05d", Integer.parseInt($rs));
                 
                 String $rt = Integer.toBinaryString(tals.get(i).rt);
-                $rt = String.format("%050d", Integer.parseInt($rt));
+                $rt = String.format("%05d", Integer.parseInt($rt));
                 
                 String $rd = Integer.toBinaryString(tals.get(i).rd);
-                $rd = String.format("%050d", Integer.parseInt($rd));
+                $rd = String.format("%05d", Integer.parseInt($rd));
                 
                 String shamt = "00000"; // zero in binary
                 
                 String funct = "100001"; // 33 in binary
+                
+                result.add(opcode + $rs + $rt + $rd + shamt + funct);
                 
             }else if (tals.get(i).instruction_id == 3){ // or, R-type, funct = 37
                 
                 String opcode = "000000"; // opcode for R-type
                 
                 String $rs = Integer.toBinaryString(tals.get(i).rs);
-                $rs = String.format("%050d", Integer.parseInt($rs));
+                $rs = String.format("%05d", Integer.parseInt($rs));
                 
                 String $rt = Integer.toBinaryString(tals.get(i).rt);
-                $rt = String.format("%050d", Integer.parseInt($rt));
+                $rt = String.format("%05d", Integer.parseInt($rt));
                 
                 String $rd = Integer.toBinaryString(tals.get(i).rd);
-                $rd = String.format("%050d", Integer.parseInt($rd));
+                $rd = String.format("%05d", Integer.parseInt($rd));
                 
                 String shamt = Integer.toBinaryString(tals.get(i).shift_amount);
-                shamt = String.format("$050d", Integer.parseInt(shamt));
+                shamt = String.format("%05d", Integer.parseInt(shamt));
                 
                 String funct = "100101"; // 37 in binary
+                
+                result.add(opcode + $rs + $rt + $rd + shamt + funct);
                 
             }else if (tals.get(i).instruction_id == 5){ // beq, I-type, opcode = 4
                 
                 String opcode = Integer.toBinaryString(4);
-                opcode = String.format("%060d", Integer.parseInt(opcode)); // gets us correct number of bits
+                opcode = String.format("%06d", Integer.parseInt(opcode)); // gets us correct number of bits
                 
                 String $rs = Integer.toBinaryString(tals.get(i).rs);
-                $rs = String.format("%050d", Integer.parseInt($rs));
+                $rs = String.format("%05d", Integer.parseInt($rs));
                 
                 String $rt = Integer.toBinaryString(tals.get(i).rt);
-                $rt = String.format("%050d", Integer.parseInt($rt));
+                $rt = String.format("%05d", Integer.parseInt($rt));
                 
                 String imm = Integer.toBinaryString(tals.get(i).immediate);
                 imm = String.format("%016d", Integer.parseInt(imm));
@@ -91,13 +96,13 @@ public class Phase3 {
             }else if (tals.get(i).instruction_id == 6){ // bne, I-type, opcode = 5
                 
                 String opcode = Integer.toBinaryString(5);
-                opcode = String.format("%060d", Integer.parseInt(opcode)); // gets us correct number of bits
+                opcode = String.format("%06d", Integer.parseInt(opcode)); // gets us correct number of bits
                 
                 String $rs = Integer.toBinaryString(tals.get(i).rs);
-                $rs = String.format("%050d", Integer.parseInt($rs));
+                $rs = String.format("%05d", Integer.parseInt($rs));
                 
                 String $rt = Integer.toBinaryString(tals.get(i).rt);
-                $rt = String.format("%050d", Integer.parseInt($rt));
+                $rt = String.format("%05d", Integer.parseInt($rt));
                 
                 String imm = Integer.toBinaryString(tals.get(i).immediate);
                 imm = String.format("%016d", Integer.parseInt(imm));
@@ -109,48 +114,51 @@ public class Phase3 {
                 String opcode = "000000"; // opcode for R-type
                 
                 String $rs = Integer.toBinaryString(tals.get(i).rs);
-                $rs = String.format("%050d", Integer.parseInt($rs));
+                $rs = String.format("%05d", Integer.parseInt($rs));
                 
                 String $rt = Integer.toBinaryString(tals.get(i).rt);
-                $rt = String.format("%050d", Integer.parseInt($rt));
+                $rt = String.format("%05d", Integer.parseInt($rt));
                 
                 String $rd = Integer.toBinaryString(tals.get(i).rd);
-                $rd = String.format("%050d", Integer.parseInt($rd));
+                $rd = String.format("%05d", Integer.parseInt($rd));
                 
                 String shamt = Integer.toBinaryString(tals.get(i).shift_amount);
-                shamt = String.format("$050d", Integer.parseInt(shamt));
+                shamt = String.format("%05d", Integer.parseInt(shamt));
                 
                 String funct = "101010"; // 42 in binary
+                
+                result.add(opcode + $rs + $rt + $rd + shamt + funct);
                 
             }else if (tals.get(i).instruction_id == 9){ // lui, I-type, opcode = 15
                 
                 String opcode = Integer.toBinaryString(15);
-                opcode = String.format("%060d", Integer.parseInt(opcode)); // gets us correct number of bits
+                opcode = String.format("%06d", Integer.parseInt(opcode)); // gets us correct number of bits
                 
                 String $rs = Integer.toBinaryString(tals.get(i).rs);
-                $rs = String.format("%050d", Integer.parseInt($rs));
+                $rs = String.format("%05d", Integer.parseInt($rs));
                 
                 String $rt = Integer.toBinaryString(tals.get(i).rt);
-                $rt = String.format("%050d", Integer.parseInt($rt));
+                $rt = String.format("%05d", Integer.parseInt($rt));
                 
                 String imm = Integer.toBinaryString(tals.get(i).immediate);
-                imm = String.format("%016d", Integer.parseInt(imm));
+                imm = imm.substring(0, 15);               
                 
                 result.add(opcode + $rs + $rt + imm);
+                
                 
             }else if (tals.get(i).instruction_id == 10){ // ori, I-type, opcode = 13
                 
                 String opcode = Integer.toBinaryString(13);
-                opcode = String.format("%060d", Integer.parseInt(opcode)); // gets us correct number of bits
+                opcode = String.format("%06d", Integer.parseInt(opcode)); // gets us correct number of bits
                 
                 String $rs = Integer.toBinaryString(tals.get(i).rs);
-                $rs = String.format("%050d", Integer.parseInt($rs));
+                $rs = String.format("%05d", Integer.parseInt($rs));
                 
                 String $rt = Integer.toBinaryString(tals.get(i).rt);
-                $rt = String.format("%050d", Integer.parseInt($rt));
+                $rt = String.format("%05d", Integer.parseInt($rt));
                 
                 String imm = Integer.toBinaryString(tals.get(i).immediate);
-                imm = String.format("%016d", Integer.parseInt(imm));
+                imm = imm.substring(0, 15);                
                 
                 result.add(opcode + $rs + $rt + imm);
                 
@@ -161,7 +169,7 @@ public class Phase3 {
             
             for(int j = 0; j < result.size(); j++){
                 // convert each string back into an int and add to output list
-                int temp = Integer.parseInt(result.get(i));
+                Integer temp = Integer.parseInt(result.get(i), 2);
                 output.add(temp);
                 
             }
